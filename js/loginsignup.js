3a562097a12a25e40dd756afa80465765955a265
s10268302 <s10268302@connect.np.edu.sg>
2025-02-04 23:18:53 +0800

    create profile + link icon to signup

--- js/loginsignup.js
+++ js/loginsignup.js
@@ -1,3 +1,5 @@
+let loginsignSuccess = false
+
 async function login(email, password) {
     const BASE_URL = 'https://fedassignment-6e81.restdb.io/rest/login';
     const API_KEY = '67939028845908919c097e5e';
@@ -21,6 +23,7 @@ async function login(email, password) {
   
       if (user) {
         alert('Login successful!');
+        loginsignSuccess=true;
       } else {
         const errorMessageElement = document.getElementById('errorMessage');
         if (errorMessageElement) {
@@ -108,6 +111,7 @@ async function login(email, password) {
         if(confirmPassword===password){
           console.log('Attempting signup with:', {email, password });
           await signup(email, password);
+          loginsignSuccess=true;
         }else if(confpw!==passwordInput){
           alert("Passwords do not match")
         }
@@ -119,3 +123,13 @@ async function login(email, password) {
       console.warn('Signup form not found.');
     }
   });
+
+  let button = document.getElementById("profileicon")
+  let isloginbutton = false
+  
+  button.addEventListener('click', function(){
+      window.location.href = "../html/signup.html";
+      if(loginsignSuccess==true){
+        
+      }
+  })
